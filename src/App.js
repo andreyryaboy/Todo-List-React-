bc438ad7c0424d3ba468ca13b1b6fa2760537b96
Andrey <atasylle@gmail.com>
2022-11-17 17:15:42 +0200

    add new todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodoHandler = (text) => {
+    setTodos([...todos, text]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} />
     </div>
   );
 }
